10063c2c7e5f6dc71db16952f2b1fe042662414e
The-Kernel-Panic <131725874+Piyush-Bhor@users.noreply.github.com>
2023-05-01 13:23:42 -0400

    Created a base template

---
+++ index.js
@@ -0,0 +1,30 @@
+// import dependencies
+const express = require('express');
+const path = require('path');
+require("dotenv").config();
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+app = express();
+
+// paths, ejs, and url setup
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended:false}));
+
+// database connection
+/*
+const url = process.env.CONNECTIONSTRING;
+mongoose.connect(url, {useNewUrlParser:true}) 
+const con = mongoose.connection
+*/
+
+//routes
+app.get('/',(req, res) => {
+    res.render('home');
+});
+
+// server start
+app.listen(8080);
+console.log('Server running at http://localhost:8080');
