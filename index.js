a88007fd4ffebfceeba34dbbf7ae144202e65ebf
The-Kernel-Panic <131725874+Piyush-Bhor@users.noreply.github.com>
2023-05-01 14:54:35 -0400

    added login & signup

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:false}));
 
 // database connection
-
 const url = process.env.CONNECTIONSTRING;
 mongoose.connect(url, {useNewUrlParser:true}) 
 const con = mongoose.connection
@@ -22,10 +21,128 @@ con.on('open', ()=> {
     console.log("Database Connnected....");
 });
 
+// user model
+const User = mongoose.model('User',{
+    username: String,
+    password: String
+});
+
+// session
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+})); 
+
 //routes
 app.get('/',(req, res) => {
     res.render('home');
 });
+
+/* login & sign up */
+app.get('/signup',(req,res) => {
+    res.render('signup_form');
+});
+
+app.get('/login',(req,res) => {
+    res.render('login_form');
+});
+
+/* login process */
+app.get('/loginprocess',(req,res) => {
+    var username = req.query.username;
+    var password = req.query.password;    
+    User.findOne({username: username, password: password}).then((user) => {
+        if(user){ 
+            // save in session
+            req.session.username = user.username;
+            req.session.loggedIn = true;
+            var pageData = {
+                login_msg : "Login Successful!"
+            }
+            res.render('login_form', pageData);
+        }
+    }).catch((err) => {
+        res.send(err);
+    });
+}); 
+
+/* logout process */
+app.get('/logout',(req,res) => {
+    req.session.username = ''; 
+    req.session.loggedIn = false;
+    var pageData = {
+        login_msg : "Logout Successful!"
+    }
+    res.render('login_form', pageData);
+});
+
+/* signup process */
+app.get('/signup_process', (req,res) => {
+    var username = req.query.username;
+    var password = req.query.password;  
+    User.findOne({username: username}).then((user) => {
+        // check if user exists
+        if(user){ 
+            var pageData = {
+                error_msg : "User Already Exists"
+            }
+            res.render('signup_form', pageData);
+        }
+        // create new user
+        else {
+            var userData = {
+                username: username,
+                password: password
+            }
+            var newUser = new User(userData);
+            newUser.save();
+            
+            var pageData = {
+                signup_msg : "User Created Successfully!"
+            }
+            res.render('signup_form', pageData);
+        }
+    }).catch((err) => {
+        res.send(err);
+    });
+});
+
+/* delete account */
+app.get('/delete_account',(req,res) => {
+    if(req.session.loggedIn) {
+        var username = req.session.username;
+        User.findOneAndDelete({username:username}).exec(function(err, user){
+            if(user){
+                //clear session cookies
+                req.session.username = ''; 
+                req.session.loggedIn = false;
+                message = "Account Deleted Successfully!";
+                var pageData = {
+                    login_msg : message
+                }
+                res.render('login_form', pageData);
+            }
+        });
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+// ------- Application Setup Stuff -------
+//        DO NOT PUSH TO PRODUCTION
+
+app.get('/setup',function(req, res){
+    var userData = {
+        username: process.env.USERNAME, 
+        password: process.env.PASSWORD 
+    }
+    var newUser = new User(userData);
+    newUser.save();
+    res.send('Done');
+});
+
 
 // server start
 app.listen(8080);
